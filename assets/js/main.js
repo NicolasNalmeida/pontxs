49f8d7626dbf950e05b76bb3830e4eeaf4c2f8e4
Nicolas <nicolas.anascimento@gmail.com>
2025-02-09 12:58:09 -0300

    feat(modal): Modal de Política de Privacidade

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,17 +22,31 @@ function modal(status) {
     
     var arr = [];    
 
-    if (status) {
+    if (status === true) {
         arr = [
             '<i class="icon success bi bi-check-lg"></i>',
             '<strong>Solicitação realizada com sucesso. Enviamos um link para o seu e-mail para o cadastro de uma nova senha.</strong>',
         ];        
-    } else {
-        arr = [
+    } else if(status === false) {
+        carr = [
             '<i class="icon warning bi bi-exclamation-triangle-fill"></i>',
             '<strong>O dado informado não foi localizado. Por favor, entre em contato em Fale Conosco.</strong>',
             '<a class="info" href="javascript:;">Fale Conosco.<a>'
         ];
+    } else if(status === 'modaltrue') {
+        arr = [
+            '<i class="icon success bi bi-check-lg"></i>',
+            '<strong class="login-true">Redefinição de senha<br> realizada com sucesso.</strong>',
+        ];
+    } else if(status === 'politica') {
+        arr = [
+            '<h1>Política de Privacidade</h1>',
+            '<h2>Na Pontxs é tudo muito claro!</h2>',
+            '<p>Temos o compromisso com a privacidade, segurança e a qualidade nos produtos e serviços prestados para os participantes durante toda experiência de premiação em nosso site.</p>',
+            '<p>Seus dados cadastrais não serão vendidos, trocados ou divulgados para terceiros, exceto quando essas informações são necessárias para o processo de entrega.</p>',
+            '<p>Suas informações são essenciais para recebimento da premiação, na sua casa, com segurança e dentro do prazo proposto de entrega. Caso existam alterações na nossa politica de privacidade você será informado através de e-mails ou neste espaço.</p>',
+        ];
+        modal.classList.add('politica');
     }
     
     textMessage.innerHTML = arr.join(' ');
