d85ba1518aefbd546b8ca878127b504499a34b94
Nicolas <nicolas.anascimento@gmail.com>
2025-03-01 19:14:12 -0300

    style: Estilo do Filtro da página de catálogo

--- assets/js/main.js
+++ assets/js/main.js
@@ -256,7 +256,34 @@ function openCloseDropdown(item) {
     openFilter.forEach(filter => {
         filter.addEventListener('click', function() {
             var content = document.getElementById(this.dataset.cat)
-            console.log(content)            
+            content.classList.toggle('open')
+            this.classList.toggle('rotate')
         })
     });
 })()
+
+$(document).ready(function () {
+    $('.input-range').each(function () {
+        var value = $(this).attr('data-slider-value');
+        var separator = value.indexOf(',');
+        if (separator !== -1) {
+            value = value.split(',');
+            value.forEach(function (item, i, arr) {
+                arr[i] = parseFloat(item);
+            });
+        } else {
+            value = parseFloat(value);
+        }
+        $(this).slider({
+            formatter: function (value) {
+                return value;
+            },
+            min: parseFloat($(this).attr('data-slider-min')),
+            max: parseFloat($(this).attr('data-slider-max')),
+            range: $(this).attr('data-slider-range'),
+            value: value,
+            tooltip_split: $(this).attr('data-slider-tooltip_split'),
+            tooltip: $(this).attr('data-slider-tooltip')
+        });
+    });
+});
